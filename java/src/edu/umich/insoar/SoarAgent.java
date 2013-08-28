e86bbbb200efcc7c25341335cab779f84d1e7060
Aaron Mininger <mininger@umich.edu>
2013-08-28 12:25:24 -0400

    Added the ability to visualize the arm in SVS and use in occlusion

--- java/src/edu/umich/insoar/SoarAgent.java
+++ java/src/edu/umich/insoar/SoarAgent.java
@@ -2,11 +2,15 @@ package edu.umich.insoar;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.util.Properties;
 
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+
+import april.config.Config;
+import april.config.ConfigFile;
 
 import sml.Agent;
 import sml.Agent.RunEventInterface;
@@ -31,6 +35,8 @@ public class SoarAgent implements RunEventInterface{
     private Kernel kernel;
     
     private WorldModel world = null;
+   
+    public String armConfig = null;
     
     public SoarAgent(String agentName, String agentSource, boolean headless){
     	kernel = Kernel.CreateKernelInNewThread();
@@ -89,6 +95,9 @@ public class SoarAgent implements RunEventInterface{
         } else {
         	lgSoarSource = null;
         }
+        
+        armConfig = props.getProperty("arm-config");
+        System.out.println("Getting Arm Config: " + armConfig);
     	
         // Source the agent
         sourceAgent(true);
@@ -107,6 +116,10 @@ public class SoarAgent implements RunEventInterface{
 	
 	public void setWorldModel(WorldModel worldModel){
 		this.world = worldModel;
+	}
+	
+	public String getArmConfig(){
+		return armConfig;
 	}
 
 	/**
