f61fba8c776ff201ebcec990a4302e2bbc3f9c5d
Aaron Mininger <mininger@umich.edu>
2015-11-11 09:27:08 -0500

    Got fetch+recall working

--- java/src/edu/umich/rosie/gui/SVSTester.java
+++ java/src/edu/umich/rosie/gui/SVSTester.java
@@ -1,19 +1,42 @@
 package edu.umich.rosie.gui;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+import edu.umich.rosie.soar.SVSCommands;
+import edu.umich.rosie.soar.SoarAgent;
 import sml.Kernel;
 import sml.Agent;
 
 public class SVSTester {
-    public static void main(String[] args) {
-        Kernel k = Kernel.CreateKernelInNewThread(64001);
-        Agent a = k.CreateAgent("soar");
-        System.out.println(a.ExecuteCommandLine("svs -e"));
-        System.out.println(a.ExecuteCommandLine("svs connect_viewer 65000"));
-        a.SendSVSInput("add snowman world p 2 2 0");
-        a.RunSelf(1);
-//      System.out.println(a.ExecuteCommandLine("svs S1.scene.sgel add snowman world p 2 2 0"));
-        while (true) {
-            //need to not exit so viewer can show stuff
-        }
+	public static void main(String[] args) {
+    	if(args.length == 0){
+    		System.err.println("SVSTester: Need path to config file as argument");
+    		System.exit(1);
+    	}
+
+        // Load the properties file
+        Properties props = new Properties();
+        try {
+			props.load(new FileReader(args[0]));
+		} catch (IOException e) {
+			System.out.println("File not found: " + args[0]);
+			e.printStackTrace();
+			return;
+		}
+        
+        new SVSTester(props);
     }
+
+	public SVSTester(Properties props){
+		SoarAgent agent = new SoarAgent(props);
+		agent.createAgent();
+		StringBuilder sb = new StringBuilder();
+		sb.append(SVSCommands.addBox("box1", new double[]{1, 1, 0}, new double[]{0.5, 0.5, 0.5}));
+		sb.append(SVSCommands.addBox("box2", new double[]{1, -0.5, 0}, new double[]{0.5, 0.5, 0.5}));
+		sb.append(SVSCommands.addBox("box3", new double[]{1, 0, 0}, new double[]{0.5, 0.5, 0.5}));
+		agent.getAgent().SendSVSInput(sb.toString());
+		while(true){}
+	}
 }
--- java/src/edu/umich/rosie/soar/SVSCommands.java
+++ java/src/edu/umich/rosie/soar/SVSCommands.java
@@ -1,14 +1,41 @@
 package edu.umich.rosie.soar;
 
-
 public class SVSCommands {
+	// double[] pos = double[]{ x, y, z }
+    public static String posToStr(double[] pos){
+    	return String.format("%f %f %f", pos[0], pos[1], pos[2]);
+    }    
     
-    public static String add(String objID, double[] pos, double[] rot, double[] size){
-    	return String.format("add %s world v %s p %s r %s s %s\n", objID, bboxVertices(), posToStr(pos), rotToStr(rot), sizeToStr(size));
+    // double[] rot = double[]{ roll, pitch, yaw }
+    public static String rotToStr(double[] rot){
+    	return String.format("%f %f %f", rot[0], rot[1], rot[2]);
+    }   
+    
+    // double[] scale = double[]{ scale_x, scale_y, scale_z }
+    public static String scaleToStr(double[] scale){
+    	return String.format("%f %f %f", scale[0], scale[1], scale[2]);
+    }   
+	
+    // Creates a set of 8 vertices forming a bounding box
+    //   of unit size centered at the origin
+    public static String bboxVertices(){
+    	return "0.5 0.5 0.5 0.5 0.5 -0.5 0.5 -0.5 0.5 0.5 -0.5 -0.5 -0.5 0.5 0.5 -0.5 0.5 -0.5 -0.5 -0.5 0.5 -0.5 -0.5 -0.5";
     }
     
-    public static String add(String objID){
+    public static String addBox(String objID){
     	return String.format("add %s world v %s\n", objID, bboxVertices());
+    }
+
+    public static String addBox(String objID, double[] pos){
+    	return String.format("add %s world v %s p %s\n", objID, bboxVertices(), posToStr(pos));
+    }
+
+    public static String addBox(String objID, double[] pos, double[] scale){
+    	return String.format("add %s world v %s p %s s %s\n", objID, bboxVertices(), posToStr(pos), scaleToStr(scale));
+    }
+
+    public static String addBox(String objID, double[] pos, double[] rot, double[] scale){
+    	return String.format("add %s world v %s p %s r %s s %s\n", objID, bboxVertices(), posToStr(pos), rotToStr(rot), scaleToStr(scale));
     }
     
     public static String changePos(String objID, double[] pos){
@@ -19,8 +46,8 @@ public class SVSCommands {
     	return String.format("change %s r %s\n", objID, rotToStr(rot));
     }
     
-    public static String changeSize(String objID, double[] size){
-    	return String.format("change %s s %s\n", objID, rotToStr(size));
+    public static String changeScale(String objID, double[] scale){
+    	return String.format("change %s s %s\n", objID, scaleToStr(scale));
     }
     
     public static String delete(String objID){
@@ -38,32 +65,4 @@ public class SVSCommands {
     public static String deleteTag(String objID, String tagName){
     	return String.format("tag delete %s %s\n", objID, tagName);
     }
-    
-    public static String bboxVertices(){
-    	StringBuilder sb = new StringBuilder();
-    	
-        for(int i = -1; i <= 1; i += 2){
-        	for(int j = -1; j <= 1; j += 2){
-        		for(int k = -1; k <= 1; k += 2){
-        			if(i > 0 || j > 0 || k > 0){
-        				sb.append(" ");
-        			}
-        			sb.append(String.format("%f %f %f", i * .5, j * .5, k * .5));
-        		}
-        	}
-        }
-        return sb.toString();
-    }
-    
-    public static String posToStr(double[] pos){
-    	return String.format("%f %f %f", pos[0], pos[1], pos[2]);
-    }    
-    
-    public static String rotToStr(double[] rot){
-    	return String.format("%f %f %f", rot[0], rot[1], rot[2]);
-    }   
-    
-    public static String sizeToStr(double[] size){
-    	return String.format("%f %f %f", size[0], size[1], size[2]);
-    }   
 }
