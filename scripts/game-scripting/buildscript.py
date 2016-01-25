e310e54e54a998bf8ba956f8d6f752fd4d0feb35
jrkirk <jrkirk@bahamut.eecs.umich.edu>
2016-01-25 01:39:38 -0500

    game permutenscript working on 8 games

--- scripts/game-scripting/buildscript.py
+++ scripts/game-scripting/buildscript.py
@@ -1,10 +1,10 @@
 import random, re, string
 
-#gamelist = ["3mens", "tictactoe"];
+#gamelist = ["8puzzle", "5puzzle", "4tower", "3tower"];
 gamelist=["3mens", "tictactoe", "8puzzle", "5puzzle", "4tower", "3tower", "picaria", "9holes"]
 shuffled = random.sample(gamelist, len(gamelist))
-newscript = str(shuffled)
-newscript += "\n"
+#newscript = str(shuffled) + "\n"
+newscript = ""
 scriptname = ""
 for game in shuffled:
     scriptname += game[0];
@@ -19,20 +19,18 @@ for line in newscript.splitlines():
     mylist = line.split(',')
     for sentence in mylist:
         if "name of an action" in sentence or "name of the goal" in sentence or "name of the action" in sentence or "name of a failure" in sentence:
-            result.write(sentence + "\n")
+            result.write(sentence + ".\n")
             if sentence in learnedlist: #don't need to learn
                 break;
             else:
                 learnedlist += sentence
-        elif "then it is" in sentence:
+        elif "then it is" in sentence or "then the location is" in sentence:
             if sentence in learnedlist:
                 #result.write("yes\n")# ASSUME unique names for test noyesalways
                 continue;
             else:
-                result.write(sentence + "\n")
+                result.write(sentence + ".\n")
                 learnedlist += sentence
         else:
-            result.write(sentence + "\n")
-
-result.write("END")
+            result.write(sentence + ".\n")
 result.close();
