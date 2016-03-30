dc81811e55ceb5b3835343cb059dbb62d9a0c69f
jrkirk <jrkirk@bahamut.eecs.umich.edu>
2016-03-30 18:21:04 -0400

    adding blocksworld

--- agent/_agent/game-scripts/run_experiment.sh
+++ agent/_agent/game-scripts/run_experiment.sh
@@ -6,6 +6,7 @@ topstate=".top"
 csv=".csv"
 dialog=".dialog"
 pertaskstats=".pertaskstats"
+
 dc=".dc"
 
 rm pertaskstats.txt
@@ -16,24 +17,24 @@ do
 	
 	cp $rfile soar-game.script
 	rfilename=${rfile%$suffix}
-	b="zf4gpt5r9o83jm"
-	if [ "$rfilename" \< "$b" ]; then
-		echo $rfilename
-		continue
-	fi
+	#b="zf4gpt5r9o83jm"
+	#if [ "$rfilename" \< "$b" ]; then
+	#	echo $rfilename
+	#	continue
+	#fi
 	#echo "oops"
 	java SentencesToSoar soar-game.script
 	cp soar-game.soar /home/jrkirk/rosie2/rosie-project/rosie/agent/_agent/language-comprehension/comprehension/test-sentences/
 	/home/jrkirk/rosie2/rosie-project/soar/out/./cli < game-data-agent.soar
-	cp stats.txt results5/$rfilename$stats
-	cp statsmax.txt results5/$rfilename$maxstats
-	cp stats.csv results5/$rfilename$csv
-	cp s1.txt results5/$rfilename$topstate
-	cp soar-game.script results5/$rfilename$dialog
+	cp stats.txt results6/$rfilename$stats
+	cp statsmax.txt results6/$rfilename$maxstats
+	cp stats.csv results6/$rfilename$csv
+	cp s1.txt results6/$rfilename$topstate
+	cp soar-game.script results6/$rfilename$dialog
 	
-	cp pertaskstats.txt results5/$rfilename$pertaskstats
-	cp smemuse.txt results5/$rfilename$smem
-	cp statsdc.txt results5/$rfilename$dc
+	cp pertaskstats.txt results6/$rfilename$pertaskstats
+	cp smemuse.txt results6/$rfilename$smem
+	cp statsdc.txt results6/$rfilename$dc
 	
 	
 	rm pertaskstats.txt
