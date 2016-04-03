5d5b5b02813646183fbb8eea25607315a072892f
jrkirk <jrkirk@bahamut.eecs.umich.edu>
2016-04-03 18:13:56 -0400

    chunk parsing stats and blocks world

--- agent/_agent/game-scripts/run_experiment.sh
+++ agent/_agent/game-scripts/run_experiment.sh
@@ -17,11 +17,12 @@ do
 	
 	cp $rfile soar-game.script
 	rfilename=${rfile%$suffix}
-	#b="zf4gpt5r9o83jm"
-	#if [ "$rfilename" \< "$b" ]; then
-	#	echo $rfilename
-	#	continue
-	#fi
+	
+	b="fg34z8mrjb95top"
+	if [ "$rfilename" \< "$b" ]; then
+		echo $rfilename
+		continue
+	fi
 	#echo "oops"
 	java SentencesToSoar soar-game.script
 	cp soar-game.soar /home/jrkirk/rosie2/rosie-project/rosie/agent/_agent/language-comprehension/comprehension/test-sentences/
