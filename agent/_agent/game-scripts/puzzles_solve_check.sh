ae68d0987757f609c23c57a1707b5db900e798cd
jrkirk <jrkirk@bahamut.eecs.umich.edu>
2017-02-08 09:45:44 -0500

    fixing bugs, cleaning code

--- agent/_agent/game-scripts/puzzles_solve_check.sh
+++ agent/_agent/game-scripts/puzzles_solve_check.sh
@@ -13,7 +13,7 @@ dot="."
 a="a"
 d="d"
 
-#declare -a arr=("lfamilycross")
+#declare -a arr=("cannibals")
 declare -a arr=("cannibals" "15ipuzzle" "3tower" "gbfox" "8puzzle4" "8puzzle5" "8puzzle6" "8puzzle6alt" "8puzzle" "5puzzle" "iso8puzzle" "yiso5puzzle" "zmaze" "blocksworld" "worldblocks" "lfamilycross" "jmahjong" "husbands")
 
 #modify less slow cannibals gbfox husbands 15ipuzzle
