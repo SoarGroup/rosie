b4023e677ecef3f4da93033c74bdff7ce98cb731
jrkirk <jrkirk@bahamut.eecs.umich.edu>
2017-05-25 14:58:57 -0400

    initial java versions of world and sentence generator

--- tools/java/src/edu/umich/rosie/tools/config/SentencesGenerator.java
+++ tools/java/src/edu/umich/rosie/tools/config/SentencesGenerator.java
@@ -5,17 +5,43 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
 
 public class SentencesGenerator {
 	
 	public static void generateRosieSentences(File inputFile, File outputFile){
 		try{
-			Writer outputWriter = new BufferedWriter(new OutputStreamWriter(
-					new FileOutputStream(outputFile), "utf-8"));
+			//Writer outputWriter = new BufferedWriter(new OutputStreamWriter(
+			//		new FileOutputStream(outputFile), "utf-8"));
 			
-			outputWriter.write("# Placeholder for auto-generated sentences file");
+			//outputWriter.write("# Placeholder for auto-generated sentences file");
+			ANTLRInputStream input;
+			
+			InputStream fileStream = Files.newInputStream(inputFile.toPath());
+			//InputStream fileStream = Files.newInputStream(Paths.get(inputFile));
+			input = new ANTLRInputStream(fileStream);
+			
+			//  Create a lexer
+			RegressLexer lexer = new RegressLexer(input);
+			//  Create a token stream
+			CommonTokenStream tokens = new CommonTokenStream(lexer);
+			//  Create a parser
+			RegressParser parser = new RegressParser(tokens);
 
-			outputWriter.close();
+			//  Now parse the input to get a parse tree
+			ParseTree tree = parser.corpus();
+
+			//  Walk the tree to generate the translation
+			ParseTreeWalker walker = new ParseTreeWalker();
+			walker.walk(new TranslateSentence(inputFile.toString(), outputFile.toString()), tree);
+
+
+			//outputWriter.close();
 		} catch (Exception e){
 			e.printStackTrace();
 		}	
