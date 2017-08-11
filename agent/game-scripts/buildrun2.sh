63940594f37dabb1e00d24c8b756ba81cde1b173
jrkirk <jrkirk@bahamut.eecs.umich.edu>
2017-08-11 12:11:35 -0400

    teaching predicates with actions

--- agent/game-scripts/buildrun2.sh
+++ agent/game-scripts/buildrun2.sh
@@ -2,6 +2,6 @@
 
 cp $ROSIE_PROJ/rosie/agent/game-scripts/soar-game.script $ROSIE_HOME/test-agents/game/example.sentences
 
-build_agent game
+/home/jrkirk/rosie2/scripts/build_agent game
 
 sh run2.sh
