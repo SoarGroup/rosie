05aeb44f396f20442afab9ad385bf2255fff3247
jrkirk <jrkirk@bahamut.eecs.umich.edu>
2019-03-21 17:11:55 -0400

    merge fixes

--- agent/game-scripts/buildnewscript.py
+++ agent/game-scripts/buildnewscript.py
@@ -64,7 +64,7 @@ gamelist=[
 
 ##"ubreakthrough",
 #xshuffled = ["Osuko","Jsujiko","Rsurvo","Dshuffle","8puzzle","xsudoku","Ukakuro"]
-shuffled = ["Ukakuro"]
+shuffled = ["8puzzle"]
 #"Ukakuro"]
 #"Ubreakthrough"]
 #"Lazystackedfrogs"]
