d66527b3fcfabe0eeeb067808d992c3d9167f846
Aaron Mininger <mininger@umich.edu>
2019-10-24 15:44:31 -0400

    Bug fixes to make sure create-scripted-sentences operator works

--- tools/java/src/edu/umich/rosie/tools/config/RosieAgentConfigurator.java
+++ tools/java/src/edu/umich/rosie/tools/config/RosieAgentConfigurator.java
@@ -32,6 +32,7 @@ public class RosieAgentConfigurator {
 			writeAgentSourceFile(config, agentSourceFilename);
 			writeSmemSourceFile(config, smemSourceFilename);
 
+
 		} catch (Exception e){
 			e.printStackTrace();
 		}
