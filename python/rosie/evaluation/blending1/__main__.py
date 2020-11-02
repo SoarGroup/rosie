6726fc73230308f2902a607ab03cd6a23a5a62a7
Aaron Mininger <mininger@umich.edu>
2020-11-02 13:05:42 -0500

    Refactored the first blended task evaluation

---
+++ python/rosie/evaluation/blending1/__main__.py
@@ -0,0 +1,23 @@
+from tkinter import *
+import tkinter.font
+
+import sys
+import os
+
+from rosie.evaluation import EvaluationGUI
+
+# Lookup $ROSIE_HOME
+rosie_home = ""
+if "ROSIE_HOME" in os.environ:
+    rosie_home = os.environ["ROSIE_HOME"]
+else:
+    print("ERROR: Requires ROSIE_HOME environment variable set")
+    sys.exit(0)
+
+agent_file = rosie_home + "/python/rosie/evaluation/blending1/agent/rosie.blending1.config"
+
+root = Tk()
+eval_gui = EvaluationGUI(agent_file, master=root)
+root.protocol("WM_DELETE_WINDOW", eval_gui.on_exit)
+root.mainloop()
+
