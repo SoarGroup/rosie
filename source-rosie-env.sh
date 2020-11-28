58edcfc743ee34d02a58462bc8d277d4b9ed4b58
Aaron Mininger <mininger@umich.edu>
2020-11-28 12:22:25 -0500

    Initial implementation of a whenever task

--- source-rosie-env.sh
+++ source-rosie-env.sh
@@ -9,6 +9,7 @@ export CLASSPATH=$CLASSPATH:$ROSIE_HOME/tools/antlr-4.5-complete.jar
 export PYTHONPATH=$PYTHONPATH:$ROSIE_HOME/python
 
 export ROSIE_EVAL=$ROSIE_HOME/python/rosie/evaluation
+export ROSIE_TESTS=$ROSIE_HOME/test-agents/task-tests
 
 
 ### Rosie Scripts
