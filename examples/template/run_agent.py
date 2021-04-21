29388e33db2ddc3bdec260ccac683da5e4414879
Aaron Mininger <mininger@umich.edu>
2021-04-21 09:46:25 -0400

    Updated the template directory

--- examples/template/run_agent.py
+++ examples/template/run_agent.py
@@ -17,25 +17,25 @@ def main():
         print("ERROR: Requires ROSIE_HOME environment variable set")
         return
 
-    AGENT_NAME = "tourguide"
+    AGENT_NAME = "template"
     agent_dir = rosie_home + "/examples/" + AGENT_NAME 
-    rosie_config = agent_dir + "/agent/rosie." + AGENT_NAME + ".config"
 
     if "--test" in sys.argv:
+        rosie_config = agent_dir + "/test-agent/rosie." + AGENT_NAME + ".config"
         run_test(rosie_config)
     else:
+        rosie_config = agent_dir + "/agent/rosie." + AGENT_NAME + ".config"
         launch_gui(rosie_config)
 
 def launch_gui(rosie_config):
     root = Tk()
     rosie_agent = MobileSimAgent(rosie_config)
-    rosie_agent.messages.append("!CMD cli pc -f")
     rosie_gui = RosieGUI(rosie_agent, master=root)
     rosie_gui.run()
 
 def run_test(rosie_config):
     rosie_agent = TestAgent(config_filename=rosie_config, write_to_stdout=True, source_output="summary",
-            task_test_output_filename='output/test-output.txt', watch_level=0, messages_file="test-script.txt")
+            task_test_output_filename='output/test-output.txt', watch_level=0)
     rosie_agent.run_test('correct-output.txt')
 
 
