f04e60a58af0d12032ccd36fe56edd098a2a0a76
Preeti Ramaraj <preetir@umich.edu>
2023-01-18 12:29:27 -0500

    Added "Rosie:" and "Instructor:" to precede msgs

--- python/rosie/events/AgentMessageSent.py
+++ python/rosie/events/AgentMessageSent.py
@@ -3,7 +3,7 @@ class AgentMessageSent:
     """ Triggered when a message is sent from the agent to the instructor
         message:str - the message that was sent """
     def __init__(self, message):
-        self.message = message
+        self.message = "Robot: " + message
 
     def __str__(self):
         return "AgentMessageSent: " + self.message 
